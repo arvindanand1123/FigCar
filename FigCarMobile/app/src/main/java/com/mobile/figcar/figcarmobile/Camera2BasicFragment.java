cd12d314066652cc3c10789954e0ef35babd18ba
Arvind Anand <arvindanand1123@gmail.com>
2018-03-17 20:09:12 -0400

    v.1.1: -Fixed Crash Error
    TODO:
    -Finish Developing the Model
    -Integrate the Model Within the app
    -Streamline parsing by throwing the code that parses the data into a function for easier application of parsing when the model is in the cloud.
    
    **Ignore unit test cases for now
    
    Signed-off-by: Arvind Anand <arvindanand1123@gmail.com>

--- FigCarMobile/app/src/main/java/com/mobile/figcar/figcarmobile/Camera2BasicFragment.java
+++ FigCarMobile/app/src/main/java/com/mobile/figcar/figcarmobile/Camera2BasicFragment.java
@@ -76,7 +76,7 @@ public class Camera2BasicFragment extends Fragment
     /**
      * Tag for the {@link Log}.
      */
-    private static final String TAG = "Camera2BasicFragment";
+    private static final String TAG = "FigCarFragment";
 
     /**
      * Camera state: Showing camera preview.
